bb5c53a1ebce80733b40e534b7499853957f4673
Marco Giordano <tuplanulla@gmail.com>
2019-05-05 09:50:41 +0200

    End course section on IoC and DI.

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -7,12 +7,9 @@ public class AnnotationBeanScopeDemoApp {
 	public static void main(String[] args) {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
-		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
-		
-		boolean result = (theCoach == alphaCoach);
-		System.out.println("The two istance are equals: " + result);
-		
+		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		theCoach.getDailyWorkout();
+		theCoach.getDailyFortune();
 		context.close();
 	}
 
